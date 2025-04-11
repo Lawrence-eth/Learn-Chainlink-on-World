c9c554b26293cae70ce9000334052ff6aa5322f0
Lawrence <lawrence.eth@outlook.com>
2025-04-11 15:15:10 +0800

    fix some UI bugs

--- frontend/src/components/lessons/IntroductionLesson.jsx
+++ frontend/src/components/lessons/IntroductionLesson.jsx
@@ -443,8 +443,12 @@ Implementation Steps:
     navigate('/course-completion');
   };
 
-  const handleExit = () => {
+  const handleBackToLessons = () => {
     navigate('/lessons');
+  };
+
+  const handleExit = () => {
+    navigate('/');
   };
 
   return (
@@ -503,6 +507,13 @@ Implementation Steps:
                   </li>
                 ))}
               </ul>
+              <button
+                onClick={handleBackToLessons}
+                className="w-full mt-4 px-4 py-2 bg-indigo-100 text-indigo-900 rounded-lg hover:bg-indigo-200 transition-colors flex items-center justify-center space-x-2"
+              >
+                <FaChevronLeft />
+                <span>Back to Lessons</span>
+              </button>
             </div>
           </div>
 
