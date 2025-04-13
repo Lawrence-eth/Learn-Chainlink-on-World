72302acbead77552626f344567069c18ace462a4
Lawrence <lawrence.eth@outlook.com>
2025-04-13 16:31:29 +0800

    Remove World ID integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import LearningApp from './components/LearningApp';
-import WorldIDAuth from './components/WorldIDAuth';
 
 function App() {
   return (
     <div className="app">
-      <WorldIDAuth />
       <LearningApp />
     </div>
   );
